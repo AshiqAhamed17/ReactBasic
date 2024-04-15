0c283b96b03dfe05426ddc20158ec4a865132ac8
Ashiq Ahamed <133097350+AshiqAhamed17@users.noreply.github.com>
2024-04-15 09:38:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+//import './App.css';
+import Chai from "./chai";
+
+function App() {
+  return (
+    <>
+    <Chai />
+    <h3> first line from App </h3>
+    <h3> Second line from App</h3>
+    
+    </>
+    
+    
+    
+  ); 
+}
+
+export default App;
---
+++ chai.js
@@ -0,0 +1,13 @@
+ 
+
+ function Chai(){
+    return(
+        <>
+        <h1> first line from chai</h1>
+        <h2> second line from chai </h2>
+        </>
+    )
+        
+ }
+
+ export default Chai
